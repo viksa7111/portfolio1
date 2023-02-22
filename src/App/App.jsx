b63f80c35f93c9cc8df4b23ba7458330fbc0fb7d
Vikas Panwar <vkparmar003@gmail.com>
2023-02-22 15:21:16 +0530

    initial commit

---
+++ src/App/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from '../Components/Header'
+import MainContent from '../Components/MainContent'
+import { Footer } from '../Components/Footer'
+
+function App() {
+
+  return (
+    <div id='body'>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+    )
+}
+
+export default App
